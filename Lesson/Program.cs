83650a64f526e30b118d0923958a5d33509184ad
Netalichka <netalichka2@gmail.com>
2022-03-28 21:57:48 +0300

    change2

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -4,9 +4,30 @@ struct Complex
 {
     public double im;
     public double re;
+    public Complex Munus(Complex complex)
+    {
+        Complex newComplex;
+        newComplex.re = re - complex.re;
+        newComplex.im = im - complex.im;
+        return newComplex;
+    }
+    static void Main() 
+    {
+          Complex complex01;
+          complex01.re = 2;
+          complex01.im = 3;
 
-}
-public Complex Munus
-{
+        Complex complex02;
+        complex02.re = 0;
+        complex02.im = -2;
 
+        Console.WriteLine(complex01);
+        Console.WriteLine(complex02);
+
+        Complex complex03 = complex02.Munus(complex01);
+        Console.WriteLine($"Результат вычитания комплексных чисел:{ complex02} - { complex01} = { complex03}");
+       
+
+        Console.ReadLine();
+    }
 }
