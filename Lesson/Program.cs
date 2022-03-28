a4941df64ac16d289e828dbc7eb470bb1a759f0a
Netalichka <netalichka2@gmail.com>
2022-03-28 20:05:43 +0300

    change2803

--- Lesson/Program.cs
+++ Lesson/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+struct Complex
+{
+    public double im;
+    public double re;
+
+}
+public Complex Munus
+{
+
+}
